2eb1703b163db5838a824e9190f34652b5eccab9
Gregoire Courtois <courtoisgregoire@gmail.com>
2019-03-27 10:31:39 +0100

    Fixed result with floats

--- level1/app.rb
+++ level1/app.rb
@@ -10,13 +10,13 @@ require_relative "practitioner"
 
 def subTotal(communication)
   @doctor = @practitioners.each {|p| p if communication.id == p.id}.first
-  sum = 0.10 + BigDecimal.new("0")
+  sum = 0.10
   sum += 0.18 if communication.color == true
-  pages_printed = communication.pages_number-1
-  pages_paid = pages_printed*0.07
-  sum += pages_paid
   sum += 0.60 if @doctor.express_delivery == true
-  sum
+  pages_printed = communication.pages_number-1
+  pages_paid = pages_printed*0.07.round(2)
+  sum += pages_paid
+  sum.to_r
 end
 
 def importJSON(filepath)
@@ -40,19 +40,22 @@ def getTotals(communications)
   communications.each do |com|
     date = Date.strptime(com.sent_at, '%Y-%m-%d').to_s
     @totals
+    subResult = subTotal(com)
       if @totals.select{|x| x[:sent_on] == date} == []
         @totals << {
           "sent_on": date,
-          "total": subTotal(com)
+          "total": subResult
         }
-    else
-      ary = @totals.find{|x| x[:sent_on] == date}
-      ary[:total] += subTotal(com)
-    end
+      else
+        ary = @totals.find{|x| x[:sent_on] == date}
+        ary[:total] += subResult
+      end
   end
-  @totals
+  @totals.each do |total|
+    total[:total] = total[:total].to_f.round(2)
+  end
 end
 
 importJSON('data.json')
 getTotals(@communications)
-exportJSON('output_2.json', @totals)
+exportJSON('output.json', @totals)
